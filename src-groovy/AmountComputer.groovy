4cbc09ffa50f33592724293c787ea0d5eb14014f
Adrien <adrien.lachambre@bonitasoft.com>
2018-10-12 13:59:22 +0200

    Ensure vehicle is known

--- src-groovy/AmountComputer.groovy
+++ src-groovy/AmountComputer.groovy
@@ -1,9 +1,13 @@
+import com.bonita.lr.model.ExpenseVehicle
 import com.bonita.lr.model.ExpenseVehicleDAO
 
 public class AmountComputer {
 
     public static float computeAmount(String vehicle, int km, ExpenseVehicleDAO dao) {
-        def expeneVehicle = dao.findByName(vehicle, 0, 100).get(0);
-        return expeneVehicle.rate * km;
+        List<ExpenseVehicle> vehicles = dao.findByName(vehicle, 0, 100)
+        if (vehicles.isEmpty()) {
+            throw new IllegalArgumentException(String.format("The vehicle %s doesn't exist", vehicle))
+        }
+        return vehicles.get(0).rate * km;
     }
 }
